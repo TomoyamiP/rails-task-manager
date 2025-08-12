4694547281e754814190f0d6c0e321b681696454
Paul Miyamoto <mfsaru@me.com>
2025-08-12 14:22:23 +0900

    added 5- As a user, I can add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+      if @task.save
+        redirect_to tasks_path
+      else
+        render :new
+      end
+  end
+end
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
 end
